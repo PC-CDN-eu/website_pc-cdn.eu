e3423e7dbba3facc28657de6c0952b51f0dc15aa
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-12-11 17:13:29 +0100

    Create randombackgroundimage.js

---
+++ cdn/yourls/randombackgroundimage.js
@@ -0,0 +1,12 @@
+var bgurl = ""https://"+window.location.hostname+"/user/plugins/random-bg/img/";
+var myArray = [bg_yourls.png','bg_fuzzy_bits.png','bg_life.png','bg_fuzzy.png','bg_diag.png', 'bg_circles.png','bg_boobs.png','bg_ants.png'];
+
+
+function RandArray(array){
+ var rand = Math.random()*array.length | 0;
+ var randSelect = array[rand];
+ return randSelect;
+ } 
+var randSelect = bgurl+RandArray(myArray); 
+
+document.body.style.backgroundImage= 'url(' + randSelect + ')';
